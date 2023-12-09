bb5312ef00d536254ac815fd0f08b95e7529e854
Mason Ayles <mayles21@georgefox.edu>
2023-12-08 21:59:42 -0800

    Updates 12/8/23

--- Edge.java
+++ Edge.java
@@ -5,35 +5,44 @@
  * @param <V> The type of the vertex labels
  * @param <E> The type of the edge label
  */
-public class Edge <V,E>
+public class Edge<V, E>
 {
-    /**
-     *
-     * @param u
-     * @param v
-     * @param label
-     */
-    public void Edge(V u, V v, E label)
-    {
+    private Vertex<V> _source;
+    private Vertex<V> _destination;
+    private E _label;
 
+    /**
+     * Constructs a new Edge with the specified source vertex, destination vertex, and label.
+     *
+     * @param source The source vertex of the edge
+     * @param destination The destination vertex of the edge
+     * @param label The label associated with the edge
+     */
+    public Edge(Vertex<V> source, Vertex<V> destination, E label)
+    {
+        this._source = source;
+        this._destination = destination;
+        this._label = label;
     }
 
     /**
+     * Retrieves the source vertex of this edge.
      *
-     * @return
+     * @return The source vertex of this edge
      */
-    public V getU()
+    public Vertex<V> getU()
     {
-
+        return _source;
     }
 
     /**
+     * Retrieves the destination vertex of this edge.
      *
-     * @return
+     * @return The destination vertex of this edge
      */
-    public V getV()
+    public Vertex<V> getV()
     {
-
+        return _destination;
     }
 
     /**
@@ -42,7 +51,7 @@ public class Edge <V,E>
      */
     public E getLabel()
     {
-
+        return _label;
     }
 
     /**
